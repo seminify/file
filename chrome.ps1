f7d8c95bd02cf7dc4c54b870ba001dfbbc8b4402
seminify <75173035+seminify@users.noreply.github.com>
2026-04-17 21:53:28 +0900

    Add script to download Chrome enterprise installer

---
+++ chrome.ps1
@@ -0,0 +1,2 @@
+$ProgressPreference = "SilentlyContinue"
+Invoke-WebRequest -Uri https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi -OutFile googlechromestandaloneenterprise64.msi
